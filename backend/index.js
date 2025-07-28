67cadad85332cafbb225b2f22920d475c59ec1bb
Atharvacoding01 <atharva@nt-powersystems.com>
2025-07-28 17:52:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -119,6 +119,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // 👈 ADD THIS
 
 connectDB().then((db) => {
   const chargers = db.collection('chargers');
